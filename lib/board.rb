c7e5739da2d1a38fa1f048da1816c68541e617b8
Bluette1 <marylene.sawyer@gmail.com>
2020-05-27 18:43:39 +0200

    adding game logic
    added code to determine win or draw

--- lib/board.rb
+++ lib/board.rb
@@ -18,12 +18,47 @@ class Board
     @board[chosen-1] = symbol
   end
  
+  def check_win
 
-  def check_draw
+    grid = [
+        [@board[0], @board[1], @board[2]],
+        [@board[3]], [@board[4], @board[5]],
+        [@board[6], @board[7], @board[8]]
+    ]
 
+    if !check_rows(grid).nil?
+        return check_rows(grid)
+    end
+
+
+    # check the columns
+    if !check_columns(grid).nil?
+        return check_columns(grid)
+    end
+
+    # check the diagonals
+    diagonals = [
+        [@board[0][0], @board[1][1], @board[2][2]],
+        [@board[0][2], @board[1][1], @board[2][0]]
+    ]
+    if !check_rows(diagonals).nil?
+        return check_rows(diagonals)
+    end
+    return nil
   end
 
-  def check_posn
-    
+  def all_equal?(row)
+    row.each_cons(2).all? { |x,y| x == y }
   end
+
+  def check_rows grid
+    grid.each { |row| return row.first if all_equal?(row) } 
+    return nil
+  end
+
+  def check_columns grid
+    grid.transpose.each { |row| return row.first if all_equal?(row) }
+    return nil
+  end
+
 end
--- lib/game.rb
+++ lib/game.rb
@@ -14,54 +14,7 @@ class Game
     # play
   end
 
-
-  
-
-  def play
-    while !@win or !@draw
-      turn_number = 0
-      winner = nil
-      # while true 
-      #   if turn_number%2 ==0 
-      #     player = 1
-      #   else 
-      #     player = 2
-      #   end
-
-      #   puts "#{player}. player turn"
-      #   print_board
-      #   puts 'Choose the number to play'
-      #   choose_number = gets.chomp
-
-      #   # Number is checked 
-      #   # If it is suitable, the number will be shown as selected on board 
-      #   # If not, it will be asked to choose another number
-      #   turn_number += 1
-
-
-
-      #   # If turn number is greater than 5, it wil be checked whether there is any winner or not
-      #   unless winner == nil and turn_number < 9
-        
-      #     puts "winner #{winner}" if winner 
-      #     puts 'It is and of game because there is no number to play' unless turn_number < 9
-      #     puts 'Do you want to play one more time?'
-      #     one_more = gets.chomp
-      #     if 'y' == one_more[0].downcase
-      #       # reset board,winner and turn number
-      #     elsif 'n' == one_more[0].downcase
-      #       break
-      #     end
-      # end
-      # end
-      # check_win
-      # check_draw
-    end
-  end
-
   def check_win
-
+    @board.check_win
   end
-
-
 end
