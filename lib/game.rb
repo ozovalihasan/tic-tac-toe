98e7c05a3f685a418cd6f7928e913bd01312c3d4
Hasan Ozovali <ozovalihasan@gmail.com>
2020-05-28 18:48:46 +0300

    Update main without methods and add current_user method into Game class

--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,7 @@ require_relative './board.rb'
 
 class Game
   attr_accessor :board, :first_player, :second_player
-  attr_reader :turn_counter, :winner
+  attr_reader :turn_counter, :winner, :current_user
   def initialize
     @board = Board.new
     @winner = false
@@ -11,8 +11,24 @@ class Game
   end
 
   def players
-    @first_player = Player.new
-    @second_player = Player.new
+    @first_player = Player.new('name', 'X')
+    @second_player = Player.new('name', 'O')
+  end
+
+  def change_symbol
+    @first_player = Player.new('name', 'O')
+    @second_player = Player.new('name', 'X')
+  end
+
+  def check_player
+    if game.turn_counter.even?
+      player = game.first_player.name
+      symbol = game.first_player.mark
+    else
+      player = game.second_player.name
+      symbol = game.second_player.mark
+    end
+    :current_user = {player => symbol} 
   end
 
   def check_win
