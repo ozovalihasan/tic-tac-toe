a7da4227db851e49fb31a54fca1e5e173f4fd8e1
praz <t.prazwal@gmail.com>
2020-06-05 12:01:45 +0200

    add and refactor tests for possible positive and negative test cases of all methods in Board class.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,6 +8,7 @@ describe Board do
   let(:win_col) { [['X', 2, 3], ['X', 5, 6], ['X', 8, 9]] }
   let(:win_diag) { [['X', 2, 3], [4, 'X', 6], [7, 8, 'X']] }
   let(:win_equal) { %w[X X X] }
+  let(:not_equal) { [1, 2, 'X'] }
   describe '#print_board' do
     it 'prints the game board' do
       expect(new_board.print_board).to eql "| 1 | 2 | 3 | \n|___|___|___| \n| 4 | 5 | 6 | \n|___|___|___| \n| 7 | 8 | 9 |\n|   |   |   |\n"
@@ -15,24 +16,19 @@ describe Board do
   end
 
   describe '#check_valid' do
-    it 'returns true if given position number is between 1 and 9.' do
+    it 'returns true if the value of board in given position is an integer.' do
       expect(new_board.check_valid(9)).to eql true
     end
-
-    it "returns false if given position number isn't between 1 and 9." do      
-      expect(new_board.check_valid(10)).to eql false
+    it 'returns false if the value of board in given position is not an integer' do
+      new_board.update_board(4, 'X')
+      expect(new_board.check_valid(4)).to eql false
     end
- 
   end
 
   describe '#update_board' do
-    it 'replaces the current item in given position with given symbol if given position number is between 1 and 9.' do
+    it 'replaces the current item in given position with given symbol.' do
       new_board.update_board(4, 'X')
       expect(new_board.instance_variable_get(:@board)).to eql [1, 2, 3, 'X', 5, 6, 7, 8, 9]
-    end
-    it 'adding new items until given position if given position number is not between 1 and 9.' do
-      new_board.update_board(11, 'X')        
-      expect(new_board.instance_variable_get(:@board)).to eql [1, 2, 3, 4, 5, 6, 7, 8, 9, nil, 'X']
     end
   end
 
@@ -41,31 +37,45 @@ describe Board do
       expect(new_board.check_rows(win_row)).to eql 'X'
     end
 
-    it 'return nil if all items in a row are not same.' do
+    it 'return nil if all items in any row are not same.' do
       expect(new_board.check_rows(win_col)).to eql nil
     end
   end
 
   describe '#check_columns' do
-    it 'checks each element in given column and return the symbol if all items are same.' do
+    it 'return the symbol if all items are same in any column.' do
       expect(new_board.check_columns(win_col)).to eql 'X'
+    end
+
+    it 'return nil if all items in any column are not same.' do
+      expect(new_board.check_columns(win_row)).to eql nil
     end
   end
 
   describe '#check_diagonals' do
-    it 'checks each element in given diagonals and return the symbol if all items are same.' do
+    it 'return the symbol if all items are same in any diagonal.' do
       expect(new_board.check_diagonals(win_diag)).to eql 'X'
+    end
+
+    it 'return nil if all items in any diagonal are not same.' do
+      expect(new_board.check_diagonals(win_col)).to eql nil
     end
   end
 
   describe '#all_equal?' do
-    it 'checks whether all elements of given array are same ' do
+    it 'returns true if all elements of given array are equal.' do
       expect(new_board.all_equal?(win_equal)).to eql true
+    end
+    it 'returns false if all elements ofgiven array are not equal.' do
+      expect(new_board.all_equal?(not_equal)).to eql false
     end
   end
 
   describe '#check_win' do
-    it 'checks whether all elements of given array are same ' do
+    it 'returns nil if any winning condition is not satisfied.' do
+      expect(new_board.check_win).to eql nil
+    end
+    it 'returns the symbol  of winner if any winning condition is satisfied ' do
       new_board.update_board(2, 'X')
       new_board.update_board(5, 'X')
       new_board.update_board(8, 'X')
